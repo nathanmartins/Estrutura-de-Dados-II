fe911642bd46a7c5c29172789972efdaf604e349
Nathan Martins <nathan.eua@gmail.com>
2019-05-13 17:20:05 -0300

    correção

--- complex_sort/src/6_heapsort.c
+++ complex_sort/src/6_heapsort.c
@@ -5,17 +5,17 @@
 
 #include<stdio.h>
 
-int heap[5], t, a;
+int heap[13] = {16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4}, t, a;
 
 void ajuste(int, int);
 
 void sort() {
-    for (int i = 5 / 2 - 1; i >= 0; i--) {
-        ajuste(5, i);
+    for (int i = 13 / 2 - 1; i >= 0; i--) {
+        ajuste(13, i);
     }
 
 
-    for (int i = 5 - 1; i >= 0; i--) {
+    for (int i = 13 - 1; i >= 0; i--) {
         t = heap[0];
         heap[0] = heap[i];
         heap[i] = t;
@@ -23,7 +23,7 @@ void sort() {
 
         printf("\nIteração #%d: ", i);
 
-        for (a = 0; a < 5; a++) {
+        for (a = 0; a < 13; a++) {
             printf("\t%d", heap[a]);
         }
     }
@@ -51,18 +51,6 @@ void ajuste(int n, int i) {
 }
 
 
-// 20 5 25 25 20
-void prePopulateStatic(int arr[]) {
-
-    arr[0] = 20;
-    arr[1] = 5;
-    arr[2] = 25;
-    arr[3] = 25;
-    arr[4] = 20;
-
-}
-
-
 // Imprimir o array
 void printArr(int arr[], int size) {
     for (int index = 0; index < size; index++) {
@@ -74,14 +62,13 @@ void printArr(int arr[], int size) {
 
 void main() {
 
-    prePopulateStatic(heap);
     printf("\n\nRaw: ");
-    printArr(heap, 5);
+    printArr(heap, 13);
 
     sort();
 
     printf("\n\nSorted: ");
-    printArr(heap, 5);
+    printArr(heap, 13);
 }
 
 

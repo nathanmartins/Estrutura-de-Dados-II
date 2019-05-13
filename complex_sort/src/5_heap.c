98ba098d6d3d149e01ba370cc28df97996916437
Nathan Martins <nathan.eua@gmail.com>
2019-05-13 16:58:23 -0300

    Adicionando atividade #5

---
+++ complex_sort/src/5_heap.c
@@ -0,0 +1,38 @@
+//
+// Created by nsm on 5/13/19.
+//
+
+#include <stdio.h>
+
+
+int check(int arr[], int n) {
+
+    for (int i = 0; i <= (n - 2) / 2; i++) {
+
+        if (arr[2 * i + 1] > arr[i]) {
+            return -1;
+        }
+        
+        if (2 * i + 2 < n && arr[2 * i + 2] > arr[i]) {
+            return -1;
+        }
+        
+    }
+
+    return 0;
+}
+
+
+int main() {
+    int arr[] = {90, 15, 10, 7, 12, 2, 7, 3};
+    int n = sizeof(arr) / sizeof(int);
+
+    if (check(arr, n) == 0) {
+        printf("è um heap \n");
+    } else {
+        printf("não è um heap\n");
+    }
+
+    return 0;
+} 
+

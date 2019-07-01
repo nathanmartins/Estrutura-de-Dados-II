6380ae08080345249f94bc29aea39504263d6ab3
Nathan S. Martins <nathan.eua@gmail.com>
2019-07-01 16:24:05 -0300

    Adding more completed activities

--- graphs/list_1/bfs.py
+++ graphs/list_1/bfs.py
@@ -1,21 +1,86 @@
 #!/usr/bin/env python3
 
 import collections
+import string
 from pprint import pprint
 
-def bfs(graph, root): 
+alphabet = dict(enumerate(string.ascii_lowercase, 0))
+
+visited = []
+
+
+def get_letter_list(d):
+    l = list()
+    for item in d:
+        l.append(alphabet[item])
+
+    return '->'.join(l)
+
+
+def breadth_first_search(graph, root):
     visited, queue = set(), collections.deque([root])
-    visited.add(root)
-    while queue: 
+
+    print("E({}), {}".format(alphabet[root], get_letter_list(queue)))
+
+    while queue:
+
         vertex = queue.popleft()
-        for neighbour in graph[vertex]: 
-            if neighbour not in visited: 
-                visited.add(neighbour) 
-                queue.append(neighbour) 
+        print("D({}), {}".format(alphabet[vertex], get_letter_list(queue)))
+
+        for neighbour in graph[vertex]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+                print("E({}), {}".format(alphabet[neighbour], get_letter_list(queue)))
+
+        # pprint(queue)
+
+
+def depth_first_search(graph, node):
+    global visited
+    if node not in visited:
+        visited.append(node)
+        print(get_letter_list(visited))
+        for n in graph[node]:
+            depth_first_search(graph, n)
+
+
+def matrix_to_list(matrix):
+    graph = {}
+    for i, node in enumerate(matrix):
+        adj = []
+        for j, connected in enumerate(node):
+            if connected:
+                adj.append(j)
+        graph[i] = adj
+    return graph
+
 
 if __name__ == '__main__':
-    graph = {0: [1, 2], 1: [2], 2: [3], 3: [1,2]} 
-    
-    pprint(graph)
-    bfs(graph, 0)
-    pprint(graph)
+    a = matrix_to_list([
+        [0, 0, 0, 1, 1, 1, ],
+        [0, 0, 0, 1, 1, 1, ],
+        [0, 0, 0, 1, 1, 1, ],
+        [1, 1, 1, 0, 0, 0, ],
+        [1, 1, 1, 0, 0, 0, ],
+        [1, 1, 1, 0, 0, 0, ]
+    ])
+
+    b = matrix_to_list([
+        [0, 1, 0, 0, 1, 1, 0, 0, 0, 0, ],
+        [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, ],
+        [0, 1, 0, 1, 0, 0, 0, 1, 0, 0, ],
+        [0, 0, 1, 0, 1, 0, 0, 0, 1, 0, ],
+        [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, ],
+        [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, ],
+        [0, 1, 0, 0, 0, 0, 0, 0, 1, 1, ],
+        [0, 0, 1, 0, 0, 1, 0, 0, 0, 1, ],
+        [0, 0, 0, 1, 0, 1, 1, 0, 0, 0, ],
+        [0, 0, 0, 0, 1, 0, 1, 1, 0, 0, ],
+    ])
+
+    # breadth_first_search(a, 0)
+    # breadth_first_search(b, 0)
+
+    # depth_first_search(a, 0)
+    depth_first_search(b, 0)
